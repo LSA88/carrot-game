e6baa89295918f39ce6726fcc41649fae78fe64e
LSA88 <88520074+LSA88@users.noreply.github.com>
2022-11-11 15:11:55 +0900

    Update field.js

--- src/field.js
+++ src/field.js
@@ -31,15 +31,14 @@ export class Field {
     const x1 = 0;
     const y1 = 0;
     const x2 = this.fieldRect.width - CARROT_SIZE;
-//    const y2 = this.fieldRect.height - CARROT_SIZE;
-    const y2 = this.fieldRect.height;// - CARROT_SIZE;
+    const y2 = this.fieldRect.height - CARROT_SIZE;
     for (let i = 0; i < count; i++) {
       const item = document.createElement("img");
       item.setAttribute("class", className);
       item.setAttribute("src", imgPath);
       item.style.position = "absolute";
       const x = randomNumber(x1, x2);
-      const y = randomNumber(CARROT_SIZE, y2);
+      const y = randomNumber(y1, y2);
       item.style.left = `${x}px`;
       item.style.top = `${y}px`;
       this.field.appendChild(item);
